c2afd2afcd23ddd8fc2508892b3ede8c67eecdf7
Zach McWhirter <toomuchwinning@Zachs-MacBook-Air.local>
2020-03-21 17:41:44 -0500

    added countByFive.js

---
+++ countByFive.js
@@ -0,0 +1,14 @@
+function countByFive(num) {
+	for (let i=0;i<=num;i+=5) {
+		console.log(i)
+	}
+}
+
+console.log("Counting to 10");
+countByFive(10);
+
+console.log("Counting to 40");
+countByFive(40);
+
+console.log("Counting to 75");
+countByFive(75);
